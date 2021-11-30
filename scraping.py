e9e14ed5e5db2278d18f2448a2945f4ab4729715
jzaragoza21 <jzaragoza21@gmail.com>
2021-11-30 12:28:05 -0700

    app

--- scraping.py
+++ scraping.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[13]:
-
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
+# Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
 # Visit the mars nasa news site
 url = 'https://redplanetscience.com'
 browser.visit(url)
+
 # Optional delay for loading the page
 browser.is_element_present_by_css('div.list_text', wait_time=1)
 
+# Convert the browser html to a soup object and then quit the browser
 html = browser.html
 news_soup = soup(html, 'html.parser')
 slide_elem = news_soup.select_one('div.list_text')
@@ -56,6 +57,7 @@ img_url_rel
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 img_url
 
+# ## Mars Facts
 df = pd.read_html('https://galaxyfacts-mars.com')[0]
 df.columns=['description', 'Mars', 'Earth']
 df.set_index('description', inplace=True)
@@ -65,10 +67,4 @@ df
 df.to_html()
 
 browser.quit()
-
-
-# In[ ]:
-
-
-
 
