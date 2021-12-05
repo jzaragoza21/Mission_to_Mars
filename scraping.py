02a5f9e371480b80bcea978f73fca7d99cfb808c
jzaragoza21 <jzaragoza21@gmail.com>
2021-12-05 15:44:27 -0700

    update

--- scraping.py
+++ scraping.py
@@ -9,9 +9,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
+    img_urls_titles = mars_hemis(browser)
 
     # Run all scraping functions and store results in a dictionary
     data = {
@@ -19,9 +20,9 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now(),
-        "hemispheres": mars_hemispheres(browser)
-        }
+        'hemispheres': img_urls_titles,
+        "last_modified": dt.datetime.now()
+    }
 
     # Stop webdriver and return data
     browser.quit()
@@ -98,38 +99,23 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
-def mars_hemispheres(browser):
-    # Set the executable path and initialize Splinter
-    #executable_path = {'executable_path': ChromeDriverManager().install()}
-    #browser = Browser('chrome', **executable_path, headless=False)
-
-    # 1. Use browser to visit the URL 
+def mars_hemis(browser):
     url = 'https://marshemispheres.com/'
     browser.visit(url)
-    #browser.visit(url + 'index.html')
 
-    # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
-
-    # 3. Write code to retrieve the image urls and titles for each hemisphere.
-    # List of hemispheres
-    links = browser.find_by_css('a.product-item img')
-
-    for i in range(len(links)):
-        hemisphere = {}
-        browser.find_by_css('a.product-item img')[i].click()
-        sample_elem = browser.links.find_by_text('Sample').first
-        hemisphere['img_url'] = sample_elem['href']
-        hemisphere['title'] = browser.find_by_css('h2.title').text
-        hemisphere_image_urls.append(hemisphere) 
+    for hemis in range(4):
+        browser.links.find_by_partial_text('Hemisphere')[hemis].click()
+        html = browser.html
+        hemi_soup = soup(html, 'html.parser')
+        title = hemi_soup.find('h2', class_='title').text
+        img_url = hemi_soup.find('li').a.get('href')
+        hemispheres = {}
+        hemispheres['img_url'] = f'https://marshemispheres.com/{img_url}'
+        hemispheres['title'] = title
+        hemisphere_image_urls.append(hemispheres)
         browser.back()
-
-    # 4. Print the list that holds the dictionary of each image url and title.
     return hemisphere_image_urls
-
-    # 5. Quit the browser
-    #browser.quit()
-
 
 if __name__ == "__main__":
 
